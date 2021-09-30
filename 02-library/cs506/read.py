93e6acbe9b742fe19762444fbc83a0de4816b493
cyuemeng <yuemeng@bu.edu>
2021-09-30 13:32:07 -0400

    read and sim (except cosine similarity)

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -3,4 +3,15 @@ def read_csv(csv_file_path):
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    
+    file = []
+    with open(csv_file_path, 'r') as a:
+        f = a.readlines()
+        for line in f:
+            data = line.split(",")
+            data = [float(i) if i.isdigit() else i for i in data]
+        file.append(data)
+        
+    
+    #raise NotImplementedError()
+    return data
